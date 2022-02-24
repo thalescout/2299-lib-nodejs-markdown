08e87831cb363a77fb57c49ebe180a9c9512fcdb
THALES DA SILVA COUTINHO <thales.coutinho@embraer.com.br>
2022-02-24 08:39:03 -0300

    n

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
+const fs = require("fs");
 
-console.log(chalk.blue('vamos começar!'));
+console.log(chalk.blue("vamos começar!"));
 
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function handleError(err) {
+  throw new Error(chalk.red(err.code, "File not found"));
 }
 
-console.log(texto(paragrafo));
+function getLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  let links = [];
+  let link;
+  while ((link = regex.exec(text)) !== null) {
+    const newLink = {
+      [link[1]]: link[2],
+    }
+    console.log(chalk.cyan(newLink));
+    links.push(newLink);
+  }
+
+  return links;
+}
+
+async function getFile(path) {
+  const encoding = "utf-8";
+  try {
+    const text = await fs.promises.readFile(path, encoding);
+    return text;
+  } catch (err) {
+    handleError(err);
+  }
+}
+
+async function FindLinks(filePath) {
+  const fileText = await getFile(filePath);
+  //console.log(chalk.green(fileText));
+  const fileLinks = getLinks(fileText);
+  console.log(chalk.yellow(fileLinks));
+}
+
+FindLinks("./arquivos/texto1.md");
